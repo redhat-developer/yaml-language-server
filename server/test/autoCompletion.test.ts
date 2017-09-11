05001699d432205778627ebc2a153e55adc848a3
Josh Pinkney <Joshpinkney@gmail.com>
2017-09-10 23:27:13 -0400

    Added ability to run tests in server

--- server/test/autoCompletion.test.ts
+++ server/test/autoCompletion.test.ts
@@ -27,109 +27,104 @@ import {schemaService, languageService}  from './testHelper';
 var glob = require('glob');
 var assert = require('assert');
 
+suite("Auto Completion Tests", () => {
 
-schemaService.getResolvedSchema(schemaService.getRegisteredSchemaIds()[0]).then(schema =>{
+	describe('Server - Auto Completion - yamlCompletion', function(){
+		
+		describe('doComplete', function(){
+			
+			// it('Autocomplete on root node without word', (done) => {
+			// 	let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+			// 	let content = "";
+			// 	let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+			// 	let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+			// 	let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(0), <YAMLDocument>yDoc2);
+			// 	let auto = new autoCompletionProvider(schema.schema);
+			// 	validator.then(function(result){
+			// 		// Commented out because of programically added snippets
+			// 		// result.items.forEach(element => {
+			// 		// 	assert.notEqual(fullWordsList.indexOf(element["label"]), -1);
+			// 		// });
+			// 		assert.equal(result.items.length, fullWords.length+5);					
+			// 	}).then(done, done);
+			// });
 
-    suite("Auto Completion Tests", () => {
+			it('Autocomplete on root node with word', (done) => {
+				let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+				let content = "apiVers:";
+				let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+				let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+				let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(6), <YAMLDocument>yDoc2);
+				validator.then(function(result){
+					assert.equal(result.items.length, 5);
+					//assert.equal(result.items[0]["label"], "apiVersion");
+				}).then(done, done);
+			});
 
-        describe('Server - Auto Completion - yamlCompletion', function(){
-            
-            describe('doComplete', function(){
-				
-				// it('Autocomplete on root node without word', (done) => {
-				// 	let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-				// 	let content = "";
-				// 	let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-				// 	let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-				// 	let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(0), <YAMLDocument>yDoc2);
-				// 	let auto = new autoCompletionProvider(schema.schema);
-				// 	validator.then(function(result){
-				// 		// Commented out because of programically added snippets
-				// 		// result.items.forEach(element => {
-				// 		// 	assert.notEqual(fullWordsList.indexOf(element["label"]), -1);
-				// 		// });
-				// 		assert.equal(result.items.length, fullWords.length+5);					
-				// 	}).then(done, done);
-				// });
+			it('Autocomplete on scalar node', (done) => {
+				let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+				let content = "kind: Deploymen";
+				let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+				let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+				let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(15), <YAMLDocument>yDoc2);
+				validator.then(function(result){
+					assert.equal(result.items.length, 105);
+					//assert.notEqual(result.items.map(x => x["label"]).indexOf("Deployment"), -1);
+				}).then(done, done);
+			});
 
-				it('Autocomplete on root node with word', (done) => {
-					let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-					let content = "apiVers:";
-					let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-					let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-					let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(6), <YAMLDocument>yDoc2);
-					validator.then(function(result){
-						assert.equal(result.items.length, 5);
-						//assert.equal(result.items[0]["label"], "apiVersion");
-					}).then(done, done);
-				});
+			/*
+				* Fix me. Need a node somehow.
+				*/
+			// it('Autocomplete on child node without word', (done) => {
+			// 	let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+			// 	let content = "metadata:\n";
+			// 	let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+			// 	let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+			// 	let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(6), <YAMLDocument>yDoc2);
+			// 	validator.then(function(result){
+			// 		assert.equal(result.items.length, 1);
+			// 		assert.equal(result.items[0]["label"], ["Deployment"]);
+			// 	}).then(done, done);
+			// });
 
-				it('Autocomplete on scalar node', (done) => {
-					let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-					let content = "kind: Deploymen";
-					let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-					let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-					let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(15), <YAMLDocument>yDoc2);
-					validator.then(function(result){
-						assert.equal(result.items.length, 105);
-						//assert.notEqual(result.items.map(x => x["label"]).indexOf("Deployment"), -1);
-					}).then(done, done);
-				});
+			it('Autocomplete on child node with word', (done) => {
+				let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+				let content = "metadata:\n  genera:";
+				let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+				let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+				let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(18), <YAMLDocument>yDoc2);
+				validator.then(function(result){
+					assert.equal(result.items.length, 5);
+					//assert.equal(result.items[0]["label"], ["generateName"]);
+				}).then(done, done);
+			});
 
-				/*
-				 * Fix me. Need a node somehow.
-				 */
-				// it('Autocomplete on child node without word', (done) => {
-				// 	let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-				// 	let content = "metadata:\n";
-				// 	let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-				// 	let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-				// 	let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(6), <YAMLDocument>yDoc2);
-				// 	validator.then(function(result){
-				// 		assert.equal(result.items.length, 1);
-				// 		assert.equal(result.items[0]["label"], ["Deployment"]);
-				// 	}).then(done, done);
-				// });
+			it('Autocomplete in the middle of file', (done) => {
+				let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+				let content = "apiVersion: v1\nallow\nmetadata:\n  generateName: hello";
+				let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+				let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+				let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(18), <YAMLDocument>yDoc2);
+				validator.then(function(result){
+					assert.equal(result.items.length, 147);
+					//assert.notEqual(result.items.map(x => x["label"]).indexOf("allowed"), -1);
+				}).then(done, done);
+			});
 
-				it('Autocomplete on child node with word', (done) => {
-					let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-					let content = "metadata:\n  genera:";
-					let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-					let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-					let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(18), <YAMLDocument>yDoc2);
-					validator.then(function(result){
-						assert.equal(result.items.length, 5);
-						//assert.equal(result.items[0]["label"], ["generateName"]);
-					}).then(done, done);
-				});
+			it('Scalar autocomplete in middle of file', (done) => {	
+				let uri = "file://~/Desktop/vscode-k8s/test.yaml";
+				let content = "apiVersion: v1\nkind: Deploymen\nmetadata:\n  name: testing";
+				let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
+				let yDoc2 = yamlLoader(testTextDocument.getText(),{});
+				let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(29), <YAMLDocument>yDoc2);
+				validator.then(function(result){
+					assert.equal(result.items.length, 105);
+					//assert.notEqual(result.items.map(x => x["label"]).indexOf("Deployment"), -1);
+				}).then(done, done);
+			});
+		});
 
-				it('Autocomplete in the middle of file', (done) => {
-					let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-					let content = "apiVersion: v1\nallow\nmetadata:\n  generateName: hello";
-					let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-					let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-					let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(18), <YAMLDocument>yDoc2);
-					validator.then(function(result){
-						assert.equal(result.items.length, 147);
-						//assert.notEqual(result.items.map(x => x["label"]).indexOf("allowed"), -1);
-					}).then(done, done);
-				});
-
-				it('Scalar autocomplete in middle of file', (done) => {	
-					let uri = "file://~/Desktop/vscode-k8s/test.yaml";
-					let content = "apiVersion: v1\nkind: Deploymen\nmetadata:\n  name: testing";
-					let testTextDocument = TextDocument.create(uri, "yaml", 1, content);
-					let yDoc2 = yamlLoader(testTextDocument.getText(),{});
-					let validator = languageService.doComplete(testTextDocument, testTextDocument.positionAt(29), <YAMLDocument>yDoc2);
-					validator.then(function(result){
-						assert.equal(result.items.length, 105);
-						//assert.notEqual(result.items.map(x => x["label"]).indexOf("Deployment"), -1);
-					}).then(done, done);
-				});
-            });
-
-        });
-
-    });
+	});
 
 });
