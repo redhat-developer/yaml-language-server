4028337027ebb935b0478814f0529f3c34daa7c3
jpinkney <josh.pinkney@mail.utoronto.ca>
2018-02-09 10:20:02 -0500

    Remove unneeded parser code

--- src/languageService/parser/jsonParser.ts
+++ src/languageService/parser/jsonParser.ts
@@ -21,18 +21,6 @@ export enum ErrorCode {
 	Undefined = 0,
 	EnumValueMismatch = 1,
 	CommentsNotAllowed = 2,
-	UnexpectedEndOfComment = 0x101,
-	UnexpectedEndOfString = 0x102,
-	UnexpectedEndOfNumber = 0x103,
-	InvalidUnicode = 0x104,
-	InvalidEscapeCharacter = 0x105,
-	InvalidCharacter = 0x106,
-	PropertyExpected = 0x201,
-	CommaExpected = 0x202,
-	ColonExpected = 0x203,
-	ValueExpected = 0x204,
-	CommaOrCloseBacketExpected = 0x205,
-	CommaOrCloseBraceExpected = 0x206
 }
 
 export enum ProblemSeverity {
