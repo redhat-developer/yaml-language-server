766db232b34ba99bfe49d0d0f3a69b7aae45f58e
Peng Xiao <pengxiao@outlook.com>
2018-11-14 15:44:17 +0800

    chore: add prettier types

--- src/languageservice/services/yamlFormatter.ts
+++ src/languageservice/services/yamlFormatter.ts
@@ -7,12 +7,12 @@
 
 import { TextDocument, Range, Position, TextEdit } from 'vscode-languageserver-types';
 import { CustomFormatterOptions } from '../yamlLanguageService';
-const prettier = require("prettier");
+import * as prettier from 'prettier';
 
 export function format(document: TextDocument, options: CustomFormatterOptions): TextEdit[] {
     const text = document.getText();
 
-	const formatted = prettier.format(text, Object.assign(options, { parser: "yaml" }));
+	const formatted = prettier.format(text, Object.assign(options, { parser: "yaml" as prettier.BuiltInParserName }));
 
     return [TextEdit.replace(Range.create(Position.create(0, 0), document.positionAt(text.length)), formatted)];
 }
