2ef0253e91150d15689b5791416f099bac963c44
Gorkem Ercan <gorkem.ercan@gmail.com>
2019-06-21 13:49:03 -0400

    [WIP] exposing public api
    Adds index and exposes some of the APIs from the
    package for easier consumption.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './languageservice/yamlLanguageService';
+export {getLanguageService as getJSONLanguageService } from 'vscode-json-languageservice';
+export * from 'vscode-languageserver-types';
+export { parse as parse7 } from './languageservice/parser/yamlParser07';
+export { parse as parse4 } from './languageservice/parser/yamlParser04';
