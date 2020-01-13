68d0f395ccc12abf9f180fa39ce49b77d52863ad
Bam Azizi <bamzi@users.noreply.github.com>
2020-01-13 09:58:10 -0500

    Update kubernetes schema to latest available version 1.17.0
    The current schema version for kubernetes is still at 1.14.0 which is very outdated

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ nls.config(process.env['VSCODE_NLS_CONFIG'] as any);
 /****************
  * Constants
  ****************/
-const KUBERNETES_SCHEMA_URL = 'https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.14.0-standalone-strict/all.json';
+const KUBERNETES_SCHEMA_URL = 'https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.17.0-standalone-strict/all.json';
 const JSON_SCHEMASTORE_URL = 'http://schemastore.org/api/json/catalog.json';
 
 /**************************
