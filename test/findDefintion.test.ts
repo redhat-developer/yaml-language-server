15892cee2ade4d552ba5614fe40d7dee047e47e2
Josh Pinkney <joshpinkney@gmail.com>
2020-07-13 09:34:49 -0400

    Fixed formatting for findDefinition
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- test/findDefintion.test.ts
+++ test/findDefintion.test.ts
@@ -12,7 +12,7 @@ suite('FindDefintion Tests', () => {
 
     describe('Jump to defintion', function () {
 
-        function findDefinitions(content: string, position: number) {
+        function findDefinitions (content: string, position: number) {
             const testTextDocument = setupTextDocument(content);
             return languageService.findDefinition(testTextDocument, testTextDocument.positionAt(position));
         }
