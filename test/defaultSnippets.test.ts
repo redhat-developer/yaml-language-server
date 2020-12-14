cef3f435acaf8a472a04254e07179753925b1e84
Yevhen Vydolob <yvydolob@redhat.com>
2020-12-14 14:54:16 +0200

    #364 add space before value from default snippets (#373)
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- test/defaultSnippets.test.ts
+++ test/defaultSnippets.test.ts
@@ -7,6 +7,7 @@ import assert = require('assert');
 import path = require('path');
 import { ServiceSetup } from './utils/serviceSetup';
 import { CompletionList } from 'vscode-languageserver';
+import { expect } from 'chai';
 
 const uri = toFsPath(path.join(__dirname, './fixtures/defaultSnippets.json'));
 const fileMatch = ['*.yml', '*.yaml'];
@@ -154,7 +155,7 @@ suite('Default Snippet Tests', () => {
       const completion = parseSetup(content, 3);
       completion
         .then(function (result) {
-          assert.equal(result.items.length, 8); // This is just checking the total number of snippets in the defaultSnippets.json
+          assert.equal(result.items.length, 9); // This is just checking the total number of snippets in the defaultSnippets.json
           assert.equal(result.items[4].label, 'longSnippet');
           // eslint-disable-next-line
           assert.equal(
@@ -170,7 +171,7 @@ suite('Default Snippet Tests', () => {
       const completion = parseSetup(content, 11);
       completion
         .then(function (result) {
-          assert.equal(result.items.length, 8); // This is just checking the total number of snippets in the defaultSnippets.json
+          assert.equal(result.items.length, 9); // This is just checking the total number of snippets in the defaultSnippets.json
           assert.equal(result.items[5].label, 'arrayArraySnippet');
           assert.equal(
             result.items[5].insertText,
@@ -252,6 +253,14 @@ suite('Default Snippet Tests', () => {
       assert.equal(result.items[0].textEdit.range.start.character, 9);
       assert.equal(result.items[0].textEdit.range.end.line, 0);
       assert.equal(result.items[0].textEdit.range.end.character, 9);
+    });
+
+    it('should add space before value on root node', async () => {
+      const content = 'name\n';
+      const result = await parseSetup(content, 4);
+      const item = result.items.find((i) => i.label === 'name');
+      expect(item).is.not.undefined;
+      expect(item.textEdit.newText).to.be.equal('name: some');
     });
   });
 });
