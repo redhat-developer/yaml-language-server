7e140bc39cb5232b20963f84315792b568043b70
Josh Pinkney <joshpinkney@gmail.com>
2020-12-18 07:44:43 -0500

    Rework the language server so that everything is testable
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

---
+++ src/languageserver/handlers/languageHandlers.ts
@@ -0,0 +1,145 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {
+  CompletionList,
+  DidChangeWatchedFilesParams,
+  DocumentFormattingParams,
+  DocumentLink,
+  DocumentLinkParams,
+  DocumentSymbolParams,
+  IConnection,
+  TextDocumentPositionParams,
+} from 'vscode-languageserver';
+import { DocumentSymbol, Hover, SymbolInformation, TextEdit } from 'vscode-languageserver-types';
+import { isKubernetesAssociatedDocument } from '../../languageservice/parser/isKubernetes';
+import { LanguageService } from '../../languageservice/yamlLanguageService';
+import { SettingsState } from '../../yamlSettings';
+import { ValidationHandler } from './validationHandlers';
+
+export class LanguageHandlers {
+  private languageService: LanguageService;
+  private yamlSettings: SettingsState;
+  private validationHandler: ValidationHandler;
+
+  constructor(
+    private readonly connection: IConnection,
+    languageService: LanguageService,
+    yamlSettings: SettingsState,
+    validationHandler: ValidationHandler
+  ) {
+    this.languageService = languageService;
+    this.yamlSettings = yamlSettings;
+    this.validationHandler = validationHandler;
+  }
+
+  public registerHandlers(): void {
+    this.connection.onDocumentLinks((params) => this.documentLinkHandler(params));
+    this.connection.onDocumentSymbol((documentSymbolParams) => this.documentSymbolHandler(documentSymbolParams));
+    this.connection.onDocumentFormatting((formatParams) => this.formatterHandler(formatParams));
+    this.connection.onHover((textDocumentPositionParams) => this.hoverHandler(textDocumentPositionParams));
+    this.connection.onCompletion((textDocumentPosition) => this.completionHandler(textDocumentPosition));
+    this.connection.onDidChangeWatchedFiles((change) => this.watchedFilesHandler(change));
+  }
+
+  documentLinkHandler(params: DocumentLinkParams): Promise<DocumentLink[]> {
+    const document = this.yamlSettings.documents.get(params.textDocument.uri);
+    if (!document) {
+      return Promise.resolve([]);
+    }
+
+    return this.languageService.findLinks(document);
+  }
+
+  /**
+   * Called when the code outline in an editor needs to be populated
+   * Returns a list of symbols that is then shown in the code outline
+   */
+  documentSymbolHandler(documentSymbolParams: DocumentSymbolParams): DocumentSymbol[] | SymbolInformation[] {
+    const document = this.yamlSettings.documents.get(documentSymbolParams.textDocument.uri);
+
+    if (!document) {
+      return;
+    }
+
+    if (this.yamlSettings.hierarchicalDocumentSymbolSupport) {
+      return this.languageService.findDocumentSymbols2(document);
+    } else {
+      return this.languageService.findDocumentSymbols(document);
+    }
+  }
+
+  /**
+   * Called when the formatter is invoked
+   * Returns the formatted document content using prettier
+   */
+  formatterHandler(formatParams: DocumentFormattingParams): TextEdit[] {
+    const document = this.yamlSettings.documents.get(formatParams.textDocument.uri);
+
+    if (!document) {
+      return;
+    }
+
+    const customFormatterSettings = {
+      tabWidth: formatParams.options.tabSize,
+      ...this.yamlSettings.yamlFormatterSettings,
+    };
+
+    return this.languageService.doFormat(document, customFormatterSettings);
+  }
+
+  /**
+   * Called when the user hovers with their mouse over a keyword
+   * Returns an informational tooltip
+   */
+  hoverHandler(textDocumentPositionParams: TextDocumentPositionParams): Promise<Hover> {
+    const document = this.yamlSettings.documents.get(textDocumentPositionParams.textDocument.uri);
+
+    if (!document) {
+      return Promise.resolve(undefined);
+    }
+
+    return this.languageService.doHover(document, textDocumentPositionParams.position);
+  }
+
+  /**
+   * Called when auto-complete is triggered in an editor
+   * Returns a list of valid completion items
+   */
+  completionHandler(textDocumentPosition: TextDocumentPositionParams): Promise<CompletionList> {
+    const textDocument = this.yamlSettings.documents.get(textDocumentPosition.textDocument.uri);
+
+    const result: CompletionList = {
+      items: [],
+      isIncomplete: false,
+    };
+
+    if (!textDocument) {
+      return Promise.resolve(result);
+    }
+    return this.languageService.doComplete(
+      textDocument,
+      textDocumentPosition.position,
+      isKubernetesAssociatedDocument(textDocument, this.yamlSettings.specificValidatorPaths)
+    );
+  }
+
+  /**
+   * Called when a monitored file is changed in an editor
+   * Re-validates the entire document
+   */
+  watchedFilesHandler(change: DidChangeWatchedFilesParams): void {
+    let hasChanges = false;
+
+    change.changes.forEach((c) => {
+      if (this.languageService.resetSchema(c.uri)) {
+        hasChanges = true;
+      }
+    });
+
+    if (hasChanges) {
+      this.yamlSettings.documents.all().forEach((document) => this.validationHandler.validate(document));
+    }
+  }
+}
--- src/languageservice/parser/isKubernetes.ts
+++ src/languageservice/parser/isKubernetes.ts
@@ -1,7 +1,25 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { FilePatternAssociation } from '../services/yamlSchemaService';
 import * as Parser from './jsonParser07';
 
 export function setKubernetesParserOption(jsonDocuments: Parser.JSONDocument[], option: boolean): void {
   for (const jsonDoc of jsonDocuments) {
     jsonDoc.isKubernetes = option;
   }
+}
+
+export function isKubernetesAssociatedDocument(textDocument: TextDocument, paths: string[]): boolean {
+  for (const path in paths) {
+    const globPath = paths[path];
+    const fpa = new FilePatternAssociation(globPath);
+
+    if (fpa.matchesPattern(textDocument.uri)) {
+      return true;
+    }
+  }
+  return false;
 }
