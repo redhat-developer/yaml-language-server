7e7c5c53a22684b876cfb56253f364e0bfac19aa
Gorkem Ercan <gorkem.ercan@gmail.com>
2021-04-30 17:19:59 -0400

    Implement custom tags support
    Implement a generic tag implementation to
    support custom tags from settings.

--- src/languageservice/utils/parseUtils.ts
+++ src/languageservice/utils/parseUtils.ts
@@ -1,6 +1,6 @@
 import { filterInvalidCustomTags } from './arrUtils';
 import { ErrorCode } from 'vscode-json-languageservice/lib/umd/jsonLanguageTypes';
-import { Tags } from 'yaml';
+import { Tags, isSeq, isMap, YAMLMap, YAMLSeq } from 'yaml';
 
 export const DUPLICATE_KEY_REASON = 'duplicate key';
 
@@ -20,30 +20,45 @@ export interface YAMLDocDiagnostic {
   code: ErrorCode;
 }
 
-export function customTagsToAdditionalOptions(customTags: string[]): Tags {
-  const yamlTags = [];
-  const filteredTags = filterInvalidCustomTags(customTags);
+class CommonTagImpl {
+  public tag: string;
+  public readonly type: string;
+  default: never;
+  constructor(tag: string, type: string) {
+    this.tag = tag;
+    this.type = type;
+  }
+  get collection(): 'map' | 'seq' | never {
+    if (this.type === 'mapping') {
+      return 'map';
+    }
+    if (this.type === 'sequence') {
+      return 'seq';
+    }
+    return undefined;
+  }
+  identify?: (value: unknown) => boolean;
+  resolve(value: string | YAMLMap | YAMLSeq): string | YAMLMap | YAMLSeq {
+    if (isMap(value) && this.type === 'mapping') {
+      return value;
+    }
+    if (isSeq(value) && this.type === 'sequence') {
+      return value;
+    }
+    if (typeof value === 'string' && this.type === 'scalar') {
+      return value;
+    }
+  }
+}
 
+export function customTagsToAdditionalOptions(customTags: string[]): Tags {
+  const tags = [];
+  const filteredTags = filterInvalidCustomTags(customTags);
   for (const tag of filteredTags) {
     const typeInfo = tag.split(' ');
     const tagName = typeInfo[0];
     const tagType = (typeInfo[1] && typeInfo[1].toLowerCase()) || 'scalar';
-    switch (tagType) {
-      case 'sequence':
-        yamlTags.push('seq');
-        break;
-      case 'mapping':
-        yamlTags.push('map');
-        break;
-      case 'scalar':
-      default:
-        yamlTags.push({
-          identify: (value) => typeof value === 'string',
-          default: true,
-          tag: tagName,
-          resolve: (str) => str,
-        });
-    }
+    tags.push(new CommonTagImpl(tagName, tagType));
   }
-  return yamlTags;
+  return tags;
 }
