73251c1b3f7b1f5476bc8432aed382be50c62ca8
Yevhen Vydolob <yvydolob@redhat.com>
2021-06-22 15:34:48 +0300

    Fix error when completion item label not string
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- src/languageservice/services/yamlCompletion.ts
+++ src/languageservice/services/yamlCompletion.ts
@@ -519,7 +519,7 @@ export class YAMLCompletion extends JSONCompletion {
         }
         collector.add({
           kind: this.getSuggestionKind(type),
-          label: value,
+          label: this.getLabelForValue(value),
           insertText: this.getInsertTextForValue(value, separatorAfter, type),
           insertTextFormat: InsertTextFormat.Snippet,
         });
@@ -967,11 +967,17 @@ export class YAMLCompletion extends JSONCompletion {
     return this.getInsertTextForValue(value, separatorAfter, type);
   }
 
-  private getLabelForValue(value: string): string {
+  private getLabelForValue(value: unknown): string {
     if (value === null) {
       return 'null'; // return string with 'null' value if schema contains null as possible value
     }
-    return value;
+    if (Array.isArray(value)) {
+      return JSON.stringify(value);
+    }
+    if (typeof value === 'object') {
+      return JSON.stringify(value);
+    }
+    return value as string;
   }
 
   /**
