27daa94333f7c0567978d891dfa5dbd334f8af09
Martin Aeschlimann <martinae@microsoft.com>
2021-10-08 08:13:23 +0200

    add web support (#539)
    * add web support
    
    * fix prettier warnings
    
    * fix prettier warning

--- test/schemaRequestHandler.test.ts
+++ test/schemaRequestHandler.test.ts
@@ -5,7 +5,6 @@
 
 import { schemaRequestHandler } from '../src/languageservice/services/schemaRequestHandler';
 import * as sinon from 'sinon';
-import * as fs from 'fs';
 import { Connection } from 'vscode-languageserver';
 import { URI } from 'vscode-uri';
 import * as chai from 'chai';
@@ -13,6 +12,7 @@ import * as sinonChai from 'sinon-chai';
 
 const expect = chai.expect;
 chai.use(sinonChai);
+import { testFileSystem } from './utils/testHelper';
 
 describe('Schema Request Handler Tests', () => {
   describe('schemaRequestHandler', () => {
@@ -20,7 +20,8 @@ describe('Schema Request Handler Tests', () => {
     let readFileStub: sinon.SinonStub;
 
     beforeEach(() => {
-      readFileStub = sandbox.stub(fs, 'readFile');
+      readFileStub = sandbox.stub(testFileSystem, 'readFile');
+      readFileStub.returns(Promise.resolve('{some: "json"}'));
     });
 
     afterEach(() => {
@@ -28,26 +29,37 @@ describe('Schema Request Handler Tests', () => {
     });
     it('Should care Win URI', async () => {
       const connection = <Connection>{};
-      const resultPromise = schemaRequestHandler(connection, 'c:\\some\\window\\path\\scheme.json', [], URI.parse(''), false);
+      const resultPromise = schemaRequestHandler(
+        connection,
+        'c:\\some\\window\\path\\scheme.json',
+        [],
+        URI.parse(''),
+        false,
+        testFileSystem
+      );
       expect(readFileStub).calledOnceWith('c:\\some\\window\\path\\scheme.json');
-      readFileStub.callArgWith(2, undefined, '{some: "json"}');
       const result = await resultPromise;
       expect(result).to.be.equal('{some: "json"}');
     });
 
     it('UNIX URI should works', async () => {
       const connection = <Connection>{};
-      const resultPromise = schemaRequestHandler(connection, '/some/unix/path/', [], URI.parse(''), false);
-      readFileStub.callArgWith(2, undefined, '{some: "json"}');
+      const resultPromise = schemaRequestHandler(connection, '/some/unix/path/', [], URI.parse(''), false, testFileSystem);
       const result = await resultPromise;
       expect(result).to.be.equal('{some: "json"}');
     });
 
     it('should handle not valid Windows path', async () => {
       const connection = <Connection>{};
-      const resultPromise = schemaRequestHandler(connection, 'A:/some/window/path/scheme.json', [], URI.parse(''), false);
+      const resultPromise = schemaRequestHandler(
+        connection,
+        'A:/some/window/path/scheme.json',
+        [],
+        URI.parse(''),
+        false,
+        testFileSystem
+      );
       expect(readFileStub).calledOnceWith(URI.file('a:/some/window/path/scheme.json').fsPath);
-      readFileStub.callArgWith(2, undefined, '{some: "json"}');
       const result = await resultPromise;
       expect(result).to.be.equal('{some: "json"}');
     });
