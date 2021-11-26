84a737b001052e2766fc02a12ae42c66f105b6f1
unknown <msivasub@win.redhat.com>
2021-11-26 16:48:58 +0530

    Fix client/registerCapability request when workspaces/didChangeWorkspaceFolder even if dynamicRegistration set to false

--- src/yamlSettings.ts
+++ src/yamlSettings.ts
@@ -84,6 +84,7 @@ export class SettingsState {
   hasConfigurationCapability = false;
   useVSCodeContentRequest = false;
   yamlVersion: YamlVersion = '1.2';
+  hasWsChangeWathedFileDynamicRegistraton = false;
 }
 
 export class TextDocumentTestManager extends TextDocuments<TextDocument> {
