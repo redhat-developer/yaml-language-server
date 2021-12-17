5fcaa1b68fa451040e2a325e1864b813e2ec26d0
sfalmo <52037539+sfalmo@users.noreply.github.com>
2021-12-17 08:20:37 +0100

    Fix completion of last property when maxProperties is set (#614)
    * Fix completion of last property when maxProperties is set
    
    * Add test for completion with maxProperties
    
    * Fix test with new behavior from upstream

--- src/languageservice/services/yamlCompletion.ts
+++ src/languageservice/services/yamlCompletion.ts
@@ -419,7 +419,9 @@ export class YamlCompletion {
   ): void {
     const matchingSchemas = doc.getMatchingSchemas(schema.schema);
     const existingKey = textBuffer.getText(overwriteRange);
-    const hasColumn = textBuffer.getLineContent(overwriteRange.start.line).indexOf(':') === -1;
+    const lineContent = textBuffer.getLineContent(overwriteRange.start.line);
+    const hasOnlyWhitespace = lineContent.trim().length === 0;
+    const hasColon = lineContent.indexOf(':') !== -1;
 
     const nodeParent = doc.getParent(node);
 
@@ -442,7 +444,7 @@ export class YamlCompletion {
             maxProperties === undefined ||
             node.items === undefined ||
             node.items.length < maxProperties ||
-            isMapContainsEmptyPair(node)
+            (node.items.length === maxProperties && !hasOnlyWhitespace)
           ) {
             for (const key in schemaProperties) {
               if (Object.prototype.hasOwnProperty.call(schemaProperties, key)) {
@@ -500,7 +502,7 @@ export class YamlCompletion {
                   }
 
                   let insertText = key;
-                  if (!key.startsWith(existingKey) || hasColumn) {
+                  if (!key.startsWith(existingKey) || !hasColon) {
                     insertText = this.getInsertTextForProperty(
                       key,
                       propertySchema,
