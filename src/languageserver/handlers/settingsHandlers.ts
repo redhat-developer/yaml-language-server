40c86a64d089ace6bce97225c274626eba4c1294
Gorkem Ercan <gorkem.ercan@gmail.com>
2022-05-16 17:00:56 -0400

    schemas for files associatied are enabled
    When a file is associated  (file.associations setting)
    with yaml language any schemas
    that correspond to the association are enabled to be
    used from schema store.

--- src/languageserver/handlers/settingsHandlers.ts
+++ src/languageserver/handlers/settingsHandlers.ts
@@ -43,6 +43,7 @@ export class SettingsHandler {
       { section: 'http' },
       { section: '[yaml]' },
       { section: 'editor' },
+      { section: 'files' },
     ]);
     const settings: Settings = {
       yaml: config[0],
@@ -52,6 +53,7 @@ export class SettingsHandler {
       },
       yamlEditor: config[2],
       vscodeEditor: config[3],
+      files: config[4],
     };
     await this.setConfiguration(settings);
   }
@@ -81,6 +83,13 @@ export class SettingsHandler {
         this.yamlSettings.schemaStoreEnabled = settings.yaml.schemaStore.enable;
         if (settings.yaml.schemaStore.url.length !== 0) {
           this.yamlSettings.schemaStoreUrl = settings.yaml.schemaStore.url;
+        }
+      }
+      if (settings.files?.associations) {
+        for (const [ext, languageId] of Object.entries(settings.files.associations)) {
+          if (languageId === 'yaml') {
+            this.yamlSettings.fileExtensions.push(ext);
+          }
         }
       }
       this.yamlSettings.yamlVersion = settings.yaml.yamlVersion ?? '1.2';
@@ -205,7 +214,11 @@ export class SettingsHandler {
         for (const fileMatch in schema.fileMatch) {
           const currFileMatch: string = schema.fileMatch[fileMatch];
           // If the schema is for files with a YAML extension, save the schema association
-          if (currFileMatch.indexOf('.yml') !== -1 || currFileMatch.indexOf('.yaml') !== -1) {
+          if (
+            this.yamlSettings.fileExtensions.findIndex((value) => {
+              return currFileMatch.indexOf(value) > -1;
+            }) > -1
+          ) {
             languageSettings.schemas.push({
               uri: schema.url,
               fileMatch: [currFileMatch],
