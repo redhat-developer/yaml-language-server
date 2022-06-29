406dfae9ab634f2437bc06d2f2d294d84ed45db2
Jim Flanagan <57458153+jim-jigx@users.noreply.github.com>
2022-06-29 07:53:42 -0700

    Update src/languageservice/services/yamlCompletion.ts
    Co-authored-by: Petr Spacek <p.spacek@email.cz>

--- src/languageservice/services/yamlCompletion.ts
+++ src/languageservice/services/yamlCompletion.ts
@@ -339,7 +339,8 @@ export class YamlCompletion {
     let overwriteRange: Range = null;
     if (areOnlySpacesAfterPosition) {
       overwriteRange = Range.create(position, Position.create(position.line, lineContent.length));
-      if (node && isScalar(node) && lineContent.match(/\w+\s+$/)) {
+      const isOnlyWhitespace = lineContent.trim().length === 0;
+      if (node && isScalar(node) && !isOnlyWhitespace) {
         // line contains part of a key with trailing spaces, adjust the overwrite range to include only the text
         const matches = lineContent.match(/^([\s-]*)[^:]+[ \t]+\n?$/);
         if (matches && matches.length > 0) {
