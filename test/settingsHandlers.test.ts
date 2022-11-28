25a3cee03bc80222070f0c10a2750d1d417112bf
msivasubramaniaan <msivasub@redhat.com>
2022-11-28 19:04:38 +0530

    mapped file association with the matched schema
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- test/settingsHandlers.test.ts
+++ test/settingsHandlers.test.ts
@@ -122,7 +122,7 @@ describe('Settings Handlers Tests', () => {
       workspaceStub.getConfiguration.resolves([{}, {}, {}, {}, { associations: { '*.bu': 'yaml' } }]);
 
       await settingsHandler.pullConfiguration();
-      expect(settingsState.fileExtensions).to.include('*.bu');
+      expect(settingsState.fileExtensions).to.include('.bu');
       expect(settingsState.fileExtensions).to.include('.yml');
       expect(settingsState.fileExtensions).to.include('.yaml');
     });
@@ -135,7 +135,7 @@ describe('Settings Handlers Tests', () => {
           "name": "Butane config schema",
           "description": "Schema to validate butane files for Fedora CoreOS",
           "fileMatch": [
-            "*.bu"
+            "test.bu"
           ],
           "url": "https://raw.githubusercontent.com/Relativ-IT/Butane-Schemas/Release/Butane-Schema.json"
         }]}`,
@@ -148,13 +148,23 @@ describe('Settings Handlers Tests', () => {
         (validationHandler as unknown) as ValidationHandler,
         {} as Telemetry
       );
-      workspaceStub.getConfiguration.resolves([{}, {}, {}, {}]);
+      workspaceStub.getConfiguration.resolves([
+        {},
+        {},
+        {},
+        {},
+        {
+          associations: {
+            '*.bu': 'yaml',
+          },
+        },
+      ]);
       const configureSpy = sinon.stub(languageService, 'configure');
       await settingsHandler.pullConfiguration();
       configureSpy.restore();
       expect(settingsState.schemaStoreSettings).deep.include({
         uri: 'https://raw.githubusercontent.com/Relativ-IT/Butane-Schemas/Release/Butane-Schema.json',
-        fileMatch: ['*.bu'],
+        fileMatch: ['test.bu'],
         priority: SchemaPriority.SchemaStore,
         name: 'Butane config schema',
         description: 'Schema to validate butane files for Fedora CoreOS',
