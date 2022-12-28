8b359def4b8ab789612fb758db6358fd8b0d91ed
Gorkem Ercan <gorkem.ercan@gmail.com>
2022-12-28 18:07:35 -0500

    Validation for map key order
    Adds a new validation that checks if the keys
    of a map are in alphabetical order.  Updates the
    tests to cover the new validation.

--- src/languageservice/services/yamlValidation.ts
+++ src/languageservice/services/yamlValidation.ts
@@ -20,6 +20,7 @@ import { Telemetry } from '../telemetry';
 import { AdditionalValidator } from './validation/types';
 import { UnusedAnchorsValidator } from './validation/unused-anchors';
 import { YAMLStyleValidator } from './validation/yaml-style';
+import { MapKeyOrderValidator } from './validation/map-key-order';
 
 /**
  * Convert a YAMLDocDiagnostic to a language server Diagnostic
@@ -64,6 +65,7 @@ export class YAMLValidation {
       if (settings.flowMapping === 'forbid' || settings.flowSequence === 'forbid') {
         this.validators.push(new YAMLStyleValidator(settings));
       }
+      this.validators.push(new MapKeyOrderValidator());
     }
     this.validators.push(new UnusedAnchorsValidator());
   }
