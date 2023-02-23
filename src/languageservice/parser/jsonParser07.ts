201d0f6ce1ec7b33a5777a76b55e3baaf4d8b2ac
msivasubramaniaan <msivasub@redhat.com>
2023-02-23 15:39:53 +0530

    change IPv6 regex pattern and updated the test cases
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- src/languageservice/parser/jsonParser07.ts
+++ src/languageservice/parser/jsonParser07.ts
@@ -59,11 +59,11 @@ export const formats = {
   },
   ipv4: {
     errorMessage: localize('ipv4FormatWarning', 'String does not match IPv4 format.'),
-    pattern: /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/gm,
+    pattern: /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/,
   },
   ipv6: {
     errorMessage: localize('ipv4FormatWarning', 'String does not match IPv6 format.'),
-    pattern: /^((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}$/gm,
+    pattern: /^([0-9a-f]|:){1,4}(:([0-9a-f.]{0,4})*){1,7}$/i,
   },
 };
 
