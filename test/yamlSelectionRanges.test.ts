b3ef93146bf5cf3621348d405fd719bbc8deca47
Tony <legendmastertony@gmail.com>
2023-06-03 11:49:43 +0800

    More selection ranges tests

--- test/yamlSelectionRanges.test.ts
+++ test/yamlSelectionRanges.test.ts
@@ -3,9 +3,22 @@ import { Position, Range, SelectionRange } from 'vscode-languageserver-types';
 import { setupTextDocument } from './utils/testHelper';
 import { getSelectionRanges } from '../src/languageservice/services/yamlSelectionRanges';
 
+function isRangesEqual(range1: Range, range2: Range): boolean {
+  return (
+    range1.start.line === range2.start.line &&
+    range1.start.character === range2.start.character &&
+    range1.end.line === range2.end.line &&
+    range1.end.character === range2.end.character
+  );
+}
+
 function expectSelections(selectionRange: SelectionRange, ranges: Range[]): void {
   for (const range of ranges) {
     expect(selectionRange.range).eql(range);
+    // Deduplicate ranges
+    while (selectionRange.parent && isRangesEqual(selectionRange.range, selectionRange.parent.range)) {
+      selectionRange = selectionRange.parent;
+    }
     selectionRange = selectionRange.parent;
   }
 }
@@ -114,6 +127,64 @@ key:
       { start: { line: 0, character: 2 }, end: { line: 0, character: 16 } },
       { start: { line: 0, character: 1 }, end: { line: 0, character: 17 } },
       { start: { line: 0, character: 0 }, end: { line: 0, character: 18 } },
+    ]);
+  });
+
+  it('selection ranges for multiple positions', () => {
+    const yaml = `
+mapping:
+  key: value
+sequence:
+  - 1
+  - null
+    `;
+    const positions: Position[] = [
+      {
+        line: 2,
+        character: 10,
+      },
+      {
+        line: 5,
+        character: 8,
+      },
+    ];
+    const document = setupTextDocument(yaml);
+    const ranges = getSelectionRanges(document, positions);
+    expect(ranges.length).equal(positions.length);
+    expectSelections(ranges[0], [
+      { start: { line: 2, character: 7 }, end: { line: 2, character: 12 } },
+      { start: { line: 2, character: 2 }, end: { line: 2, character: 12 } },
+      { start: { line: 1, character: 0 }, end: { line: 2, character: 12 } },
+    ]);
+    expectSelections(ranges[1], [
+      { start: { line: 5, character: 4 }, end: { line: 5, character: 8 } },
+      { start: { line: 4, character: 2 }, end: { line: 5, character: 8 } },
+      { start: { line: 3, character: 0 }, end: { line: 5, character: 8 } },
+    ]);
+  });
+
+  it('selection ranges for multiple documents', () => {
+    const yaml = `
+document1:
+  key: value
+---
+document2:
+  - 1
+  - null
+      `;
+    const positions: Position[] = [
+      {
+        line: 5,
+        character: 5,
+      },
+    ];
+    const document = setupTextDocument(yaml);
+    const ranges = getSelectionRanges(document, positions);
+    expect(ranges.length).equal(positions.length);
+    expectSelections(ranges[0], [
+      { start: { line: 5, character: 4 }, end: { line: 5, character: 5 } },
+      { start: { line: 5, character: 2 }, end: { line: 6, character: 8 } },
+      { start: { line: 4, character: 0 }, end: { line: 6, character: 8 } },
     ]);
   });
 });
