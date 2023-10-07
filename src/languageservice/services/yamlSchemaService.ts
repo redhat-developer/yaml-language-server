ed03cbf71ade29ea62b4bcac0d8952195fd6969d
Remco Haszing <remcohaszing@gmail.com>
2023-10-07 18:38:07 +0200

    Remove 'use strict'
    TypeScript adds this for us.

--- src/languageservice/services/yamlSchemaService.ts
+++ src/languageservice/services/yamlSchemaService.ts
@@ -3,7 +3,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-'use strict';
 
 import { JSONSchema, JSONSchemaMap, JSONSchemaRef } from '../jsonSchema';
 import { SchemaPriority, SchemaRequestService, WorkspaceContextService } from '../yamlLanguageService';
