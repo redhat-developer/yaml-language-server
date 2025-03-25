07b54f0d585fd18537512411b551c3c6c9c64684
Petr Spacek <p.spacek@email.cz>
2025-03-25 21:59:25 +0100

    fix: do not suggest propertyNames if doNotSuggest is true  - PR: https://github.com/redhat-developer/yaml-language-server/pull/1045

--- test/autoCompletionFix.test.ts
+++ test/autoCompletionFix.test.ts
@@ -2230,6 +2230,22 @@ test1:
     expect(completion.items[0].documentation).to.be.equal('Property Description');
   });
 
+  it('should not suggest propertyNames with doNotSuggest', async () => {
+    const schema: JSONSchema = {
+      type: 'object',
+      additionalProperties: true,
+      propertyNames: {
+        title: 'property',
+        doNotSuggest: true,
+      },
+    };
+    schemaProvider.addSchema(SCHEMA_ID, schema);
+    const content = '';
+    const completion = await parseSetup(content, 0, content.length);
+
+    expect(completion.items.length).equal(0);
+  });
+
   describe('Deprecated schema', () => {
     it('should not autocomplete deprecated schema - property completion', async () => {
       const schema: JSONSchema = {
