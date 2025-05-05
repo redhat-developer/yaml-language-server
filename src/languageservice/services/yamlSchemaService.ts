12ea8a540be3fcc9ae29898715a0737229222b8d
David Thompson <davthomp@redhat.com>
2025-05-05 16:31:26 -0400

    Support draft-04 schemas
    This helps to support the schema for openapi 3.0.0,
    among others, that use an older draft of JSON schema that has some type
    differences that make it incompatible with JSON schema draft 07.
    
    See #1006 , I think the root cause for the issues that PR caused
    is that we were trying to download and cache the metaschema from
    the "URL" instead of using the copy that's bundled with `ajv`.
    
    Fixes #780, Fixes #752
    
    (and many, many duplicates we'll have to find and clean up)
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- src/languageservice/services/yamlSchemaService.ts
+++ src/languageservice/services/yamlSchemaService.ts
@@ -28,9 +28,11 @@ import { JSONSchemaDescriptionExt } from '../../requestTypes';
 import { SchemaVersions } from '../yamlTypes';
 
 import Ajv, { DefinedError } from 'ajv';
+import Ajv4 from 'ajv-draft-04';
 import { getSchemaTitle } from '../utils/schemaUtils';
 
 const ajv = new Ajv();
+const ajv4 = new Ajv4();
 
 const localize = nls.loadMessageBundle();
 
@@ -38,6 +40,10 @@ const localize = nls.loadMessageBundle();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jsonSchema07 = require('ajv/dist/refs/json-schema-draft-07.json');
 const schema07Validator = ajv.compile(jsonSchema07);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jsonSchema04 = require('ajv-draft-04/dist/refs/json-schema-draft-04.json');
+const schema04Validator = ajv4.compile(jsonSchema04);
 
 export declare type CustomSchemaProvider = (uri: string) => Promise<string | string[]>;
 
@@ -164,9 +170,10 @@ export class YAMLSchemaService extends JSONSchemaService {
     let schema: JSONSchema = schemaToResolve.schema;
     const contextService = this.contextService;
 
-    if (!schema07Validator(schema)) {
+    const validator = this.normalizeId(schema.$schema) === ajv4.defaultMeta() ? schema04Validator : schema07Validator;
+    if (!validator(schema)) {
       const errs: string[] = [];
-      for (const err of schema07Validator.errors as DefinedError[]) {
+      for (const err of validator.errors as DefinedError[]) {
         errs.push(`${err.instancePath} : ${err.message}`);
       }
       resolveErrors.push(`Schema '${getSchemaTitle(schemaToResolve.schema, schemaURL)}' is not valid:\n${errs.join('\n')}`);
