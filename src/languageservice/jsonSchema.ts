2086df4edd5082a717310eb3290cf0396ef0ce99
msivasubramaniaan <msivasub@redhat.com>
2025-06-12 16:19:24 +0530

    removed enumMarkdown entries

--- src/languageservice/jsonSchema.ts
+++ src/languageservice/jsonSchema.ts
@@ -49,8 +49,6 @@ export interface JSONSchema {
   not?: JSONSchemaRef;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   enum?: any[];
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  enumMarkdown?: any[];
   format?: string;
 
   // schema draft 06
